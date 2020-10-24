01ff7715c32c21c13fa85a1909538249a3379b64
Blake Watters <blake@opsani.com>
2020-10-24 08:38:28 -0700

    Update Docker setup to be multi-servo aware

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-# Allow literal or volume mounted tokens
-exec servo \
+if [ $(grep -c '\-\-\-' ${SERVO_CONFIG_FILE:-/servo/servo.yaml}) -ne 0 ]; then
+  # In multi-servo config everything has to be in the file
+  exec servo \
     --config-file ${SERVO_CONFIG_FILE:-/servo/servo.yaml} \
     "$@"
+else
+  # Allow literal or volume mounted tokens
+  exec servo \
+    --optimizer ${OPSANI_OPTIMIZER:?must be configured} \
+    --config-file ${SERVO_CONFIG_FILE:-/servo/servo.yaml} \
+    $(if [ ! -z ${OPSANI_TOKEN} ]; then \
+        echo "--token ${OPSANI_TOKEN}"; \
+      else \
+        echo "--token-file /servo/opsani.token"; \
+      fi) \
+    "$@"
+fi
